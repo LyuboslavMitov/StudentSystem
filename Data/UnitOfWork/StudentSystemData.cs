b2b6b0c1916ae460836f691a64614487cd2749cb
LyuboslavMitov <lyuboslav.mitov@gmail.com>
2016-01-14 17:19:03 +0200

    nvm

---
+++ Data/UnitOfWork/StudentSystemData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Data.UnitOfWork
+{
+   public class StudentSystemData                                 //Nasledi IstudentSystemData ii definirai repositoryta
+    {
+                                   
+    }
+}
