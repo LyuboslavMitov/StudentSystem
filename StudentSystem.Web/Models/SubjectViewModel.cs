994b422708f1825da5c821e9afc5a7cbbc6b5711
LyuboslavMitov <lyuboslav.mitov@gmail.com>
2016-02-18 15:12:02 +0200

    SubjectViewModel

---
+++ StudentSystem.Web/Models/SubjectViewModel.cs
@@ -0,0 +1,28 @@
+﻿using StudentSystem.DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System;
+namespace StudentSystem.Web.Models
+{
+    public class SubjectViewModel
+    {
+        [Key]
+        public int SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public static Expression<Func<Subject, SubjectViewModel>> FromSubjectModel            //Subject View Model
+        {
+            get
+            {
+                return x => new SubjectViewModel
+                {
+                    SubjectID = x.SubjectID,
+                    SubjectName = x.SubjectName,
+                };
+            }
+        }
+    }
+}
